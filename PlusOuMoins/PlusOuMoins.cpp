629b7e8dd74ea8ef4f30fe0dcd0e878c4a6a8a29
Blaireau <7900998+Blaireau@users.noreply.github.com>
2021-11-12 16:09:11 +0100

    v0.4 is ready !
    v0.4 is ready !

--- PlusOuMoins/PlusOuMoins.cpp
+++ PlusOuMoins/PlusOuMoins.cpp
@@ -4,13 +4,21 @@
 
 using namespace std;
 
+// Simple function to print rules !
+void rules() {
+    cout << "Rules are simple : The game is going to choose a number and you have to find it !\nNumber are choosen between 1 and 100 !\Good Luck !" << endl;
+
+}
+
 int main()
 {
     srand((unsigned)time(0));
     int secretValue;
     int userAnswer;
-    cout << "Shall We Play A Game v0.3\nAdded Functionnality :\n\t- Now with random value ! (Previously, it was 25)" << endl;
-    cout << "Previous version :\n\t v0.2 : Added a loop to submit multiple answer !\n\t v0.1 : First version\n" << endl;
+    cout << "Shall We Play A Game v0.4\nAdded Functionnality :\n\t- Rules display !" << endl;
+    cout << "Previous version :\n\t v0.3 : Now with random number selection !\n\t v0.2 : Added a loop to submit multiple answer !\n\t v0.1 : First version\n" << endl;
+
+    rules();
 
     secretValue = (rand() % 100) + 1;
 
